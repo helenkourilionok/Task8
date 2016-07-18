399e48a3091f12d3259b32a74a7a05e8bd43f049
Helen_Kourilionok <julia-lena-dru@yandex.ru>
2016-07-18 09:37:58 +0400

    commit jdbc filmstore

--- src/by/training/filmstore/dao/ActorDAO.java
+++
@@ -1,8 +0,0 @@
-package by.training.filmstore.dao;
-
-import by.training.filmstore.dao.exception.FilmStoreDAOException;
-import by.training.filmstore.entity.Actor;
-
-public interface ActorDAO extends AbstractDAO<Actor, Short> {
-	Actor findByFIO(String fio) throws FilmStoreDAOException;
-}
--- src/by/training/filmstore/dao/impl/FindCommentCriteria.java
+++
@@ -1,5 +0,0 @@
-package by.training.filmstore.dao.impl;
-
-public enum FindCommentCriteria {
-	FIND_BY_USER_ID,FIND_BY_FILM_ID,FIND_ALL
-}
